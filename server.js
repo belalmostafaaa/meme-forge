ff4989eabe5f60a8eb5c110d9fc4166b49518891
belalmostafaaa <116195390+belalmostafaaa@users.noreply.github.com>
2025-07-22 20:00:52 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,40 +1,34 @@
 const express = require('express');
 const http = require('http');
-const socketIO = require('socket.io');
+const { Server } = require('socket.io');
 const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIO(server);
+const io = new Server(server);
 
-const PORT = process.env.PORT || 8080;
+app.use(express.static(path.join(__dirname, 'public'))); // public folder contains index.html, client.js
 
-// Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Store the latest canvas state
-let canvasState = null;
+let currentCanvasState = null;
 
 io.on('connection', (socket) => {
-  console.log('A user connected');
+  console.log(' A user connected');
 
-  // Send the current canvas state to the new user
-  if (canvasState) {
-    socket.emit('canvas:update', canvasState);
+  if (currentCanvasState) {
+    socket.emit('canvas:update', currentCanvasState);
   }
 
-  // When receiving a canvas update from a user
   socket.on('canvas:update', (data) => {
-    canvasState = data; // Save the latest version
-    socket.broadcast.emit('canvas:update', data); // Send it to all other users
+    currentCanvasState = data;
+    socket.broadcast.emit('canvas:update', data);
   });
 
   socket.on('disconnect', () => {
-    console.log('A user disconnected');
+    console.log(' A user disconnected');
   });
 });
 
-// Start the server
+const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(` Server listening on http://localhost:${PORT}`);
 });
