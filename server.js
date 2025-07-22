b06347e2f0898eafabed4e2354cd4a85e454d810
belalmostafaaa <116195390+belalmostafaaa@users.noreply.github.com>
2025-07-22 20:14:56 +0300

    Update server.js

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ io.on('connection', (socket) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080; 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
