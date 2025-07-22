fff5db7763380131cfbe8c9a53666ed6f9578ecd
belalmostafaaa <116195390+belalmostafaaa@users.noreply.github.com>
2025-07-22 19:46:39 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
